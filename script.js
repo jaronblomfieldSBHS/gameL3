5badc96e888b3e78cec4b8402ebf0b7f047b0570
jaronblomfieldSBHS <80649752+jaronblomfieldSBHS@users.noreply.github.com>
2023-08-30 15:10:23 +1200

    made buttons aestheitcallty pleasing

--- script.js
+++ script.js
@@ -1,126 +1,120 @@
-// Questions that will be asked
-const Questions = [{
-        q: "q1",
-        a: [{
-                text: "a1",
-                isCorrect: false
-            },
-            {
-                text: "a2",
-                isCorrect: false
-            },
-            {
-                text: "a3",
-                isCorrect: true
-            },
-            {
-                text: "a4",
-                isCorrect: false
-            }
-        ]
+    const Questions = [{
+            q: "q1",
+            a: [{
+                    text: "a1",
+                    isCorrect: false
+                },
+                {
+                    text: "a2",
+                    isCorrect: false
+                },
+                {
+                    text: "a3",
+                    isCorrect: true
+                },
+                {
+                    text: "a4",
+                    isCorrect: false
+                }
+            ]
+        },
+        {
+            q: "q2",
+            a: [{
+                    text: "a1",
+                    isCorrect: false
+                },
+                {
+                    text: "a2",
+                    isCorrect: false
+                },
+                {
+                    text: "a3",
+                    isCorrect: false
+                },
+                {
+                    text: "a4",
+                    isCorrect: true
+                }
+            ]
+        },
+        {
+            q: "q3",
+            a: [{
+                    text: "a1",
+                    isCorrect: false
+                },
+                {
+                    text: "a2",
+                    isCorrect: false
+                },
+                {
+                    text: "a3",
+                    isCorrect: true
+                },
+                {
+                    text: "a4",
+                    isCorrect: false
+                }
+            ]
+        }
+    ];
 
-    },
-    {
-        q: "q2",
-        a: [{
-                text: "a1",
-                isCorrect: false,
-                isSelected: false
-            },
-            {
-                text: "a2",
-                isCorrect: false
-            },
-            {
-                text: "a3",
-                isCorrect: false
-            },
-            {
-                text: "a4",
-                isCorrect: true
-            }
-        ]
+    let currQuestion = 0;
+    let score = 0;
 
-    },
-    {
-        q: "q3",
-        a: [{
-                text: "a1",
-                isCorrect: false
-            },
-            {
-                text: "a2",
-                isCorrect: false
-            },
-            {
-                text: "a3",
-                isCorrect: true
-            },
-            {
-                text: "a4",
-                isCorrect: false
-            }
-        ]
+    function loadQues() {
+        const question = document.getElementById("ques");
+        const opt = document.getElementById("opt");
 
+        question.textContent = Questions[currQuestion].q;
+        opt.innerHTML = "";
+
+        for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+            const choicesdiv = document.createElement("div");
+            const choice = document.createElement("input");
+            const choiceLabel = document.createElement("label");
+
+            choice.type = "radio"; // Change input type to radio
+            choice.name = "answer";
+            choice.value = i;
+            choice.classList.add("radio-button"); // Apply the custom styling
+
+            choiceLabel.classList.add("radio-button-label");
+            choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+            choicesdiv.appendChild(choice);
+            choicesdiv.appendChild(choiceLabel);
+            opt.appendChild(choicesdiv);
+        }
     }
 
-]
+    loadQues();
 
-let currQuestion = 0
-let score = 0
-
-function loadQues() {
-    const question = document.getElementById("ques")
-    const opt = document.getElementById("opt")
-
-    question.textContent = Questions[currQuestion].q;
-    opt.innerHTML = ""
-
-    for (let i = 0; i < Questions[currQuestion].a.length; i++) {
-        const choicesdiv = document.createElement("div");
-        const choice = document.createElement("input");
-        const choiceLabel = document.createElement("label");
-
-        choice.type = "radio";
-        choice.name = "answer";
-        choice.value = i;
-
-        choiceLabel.textContent = Questions[currQuestion].a[i].text;
-
-        choicesdiv.appendChild(choice);
-        choicesdiv.appendChild(choiceLabel);
-        opt.appendChild(choicesdiv);
+    function loadScore() {
+        const totalScore = document.getElementById("score");
+        totalScore.textContent = `You scored ${score} out of ${Questions.length}`;
     }
-}
 
-loadQues();
-
-function loadScore() {
-    const totalScore = document.getElementById("score")
-    totalScore.textContent = `You scored ${score} out of ${Questions.length}`
-}
-
-
-function nextQuestion() {
-    if (currQuestion < Questions.length - 1) {
-        currQuestion++;
-        loadQues();
-    } else {
-        document.getElementById("opt").remove()
-        document.getElementById("ques").remove()
-        document.getElementById("btn").remove()
-        loadScore();
+    function nextQuestion() {
+        if (currQuestion < Questions.length - 1) {
+            currQuestion++;
+            loadQues();
+        } else {
+            // Hide the question and options, show the score
+            document.getElementById("opt").style.display = "none";
+            document.getElementById("ques").style.display = "none";
+            document.getElementById("btn").style.display = "none";
+            loadScore();
+        }
     }
-}
 
-function checkAns() {
-    const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+    function checkAns() {
+        const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
 
-    if (Questions[currQuestion].a[selectedAns].isCorrect) {
-        score++;
-        console.log("Correct")
-        nextQuestion();
-    } else {
+        if (Questions[currQuestion].a[selectedAns].isCorrect) {
+            score++;
+        }
+
         nextQuestion();
     }
-}
