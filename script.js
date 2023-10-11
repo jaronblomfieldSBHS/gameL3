5745a96f30542ae66c886d338db3c065eeee14ad
jaronblomfieldSBHS <80649752+jaronblomfieldSBHS@users.noreply.github.com>
2023-10-11 15:07:20 +1300

    added player sprite

--- script.js
+++ script.js
@@ -139,16 +139,17 @@
 
     const canvas = document.getElementById("gameCanvas");
     const ctx = canvas.getContext("2d");
-    let squareX = canvas.width / 2;
-    let squareY = canvas.height / 2;
-    const squareSize = 30;
+    let playerX = 500;
+    let playerY = 500;
+    const playerSize = 30;
     const moveAmount = 10;
     let healthPoints = 1000; // Initialize player's health points
 
-    function drawSquare() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "red";
-        ctx.fillRect(squareX, squareY, squareSize, squareSize);
+    var player = new Image();
+    player.src = "images/catsprite1.png";
+
+    function drawplayer() {
+        ctx.drawImage(player, playerX, playerY, playerSize, playerSize);
 
         // Display health points on the canvas
         ctx.fillStyle = "white";
@@ -156,30 +157,34 @@
         ctx.fillText(`Health: ${healthPoints}`, 10, 30);
     }
 
+    function drawImg(src, x, y, w, h) {
+        canvasContext.drawImage(src, x, y, w, h);
+    }
+
     function handleKey(event) {
         switch (event.key) {
             case "w":
-                squareY -= moveAmount;
+                playerY -= moveAmount;
                 break;
             case "s":
-                squareY += moveAmount;
+                playerY += moveAmount;
                 break;
             case "a":
-                squareX -= moveAmount;
+                playerX -= moveAmount;
                 break;
             case "d":
-                squareX += moveAmount;
+                playerX += moveAmount;
                 break;
         }
 
-        // Ensure the square stays within the canvas boundaries
-        squareX = Math.max(0, Math.min(squareX, canvas.width - squareSize));
-        squareY = Math.max(0, Math.min(squareY, canvas.height - squareSize));
+        // Ensure the player stays within the canvas boundaries
+        playerX = Math.max(0, Math.min(playerX, canvas.width - playerSize));
+        playerY = Math.max(0, Math.min(playerY, canvas.height - playerSize));
 
-        drawSquare();
+        drawplayer();
     }
 
-    drawSquare();
+    drawplayer();
 
     // Listen for key presses
     window.addEventListener("keydown", handleKey);
@@ -192,5 +197,5 @@
             alert("Game over! Your health points reached " + healthPoints);
             location.reload(); // Reload the game
         }
-        drawSquare();
+        drawplayer();
     }
